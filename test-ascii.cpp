e6734795ff3ff9ce7461d6cfe7ed4dcd80932e49
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-10-13 08:55:29 -0400

    Create test-ascii.cpp

---
+++ test-ascii.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string a = ""; 
+  std::cout << "Input: ";
+  getline(std::cin, a);
+    std::cout << "Your line was: " << a << std::endl;
+ 
+  for (int i = 0 ; i < a.length(); i++){
+     
+     std::cout << a.substr(i , 1) <<  " " << int(a[i])<<  std::endl;
+    
+     }
+  }
+ 
