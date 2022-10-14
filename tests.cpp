2e9ab206c460b89a1329df7923aaa9d9c943745b
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-10-14 05:19:29 -0400

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,8 +1,21 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-#include "funcs.h"
+
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
+
 
 TEST_CASE("Caesar Cipher Encryption") {
-CHECK(encryptCaesar("Hello, World!", 10) == ""Hello, World!"");
-CHECK(fact(1) == 1);
+CHECK(encryptCaesar("Hello, World!", 10) == "Hello, World!");
+CHECK(encryptCaesar("Way to Go!", 5)== "Bfd yt Lt!");
 }
+
+TEST_CASE("Vigenere Cipher Encryption"){
+CHECK(encryptVigenere("Hello, World!","cake") == "Jevpq, Wyvnd!");
+CHECK(encryptVigenere("I am safe!","dog") == "O da ydtk!");  
+}
+TEST_CASE("Decryption"){
+
+  }
+
+
